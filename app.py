84a8990a0375da5c86af6ad761063ae4f6e5822e
Pushpender Singh <singhpushpender250@gmail.com>
2025-05-24 23:26:31 +0530

    a

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style.css")
+# local_css("style.css")
 
 # Page selection in sidebar
 st.sidebar.title("Navigation")
